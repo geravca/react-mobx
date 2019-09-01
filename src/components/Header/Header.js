b36badba59da84e185d9d6ecd3227d76d64d3bb7
Gerardo Contreras <>
2019-08-31 22:41:16 -0600

    back button added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,45 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import {Col, Container, Jumbotron, Row} from 'reactstrap';
+import {Button, Col, Container, Jumbotron, Row} from 'reactstrap';
 import ClockTime from '../Clock/Clock';
 
 import styles from './Header.scss';
 import {inject, observer} from 'mobx-react';
 import logo from '../../logo.svg';
+import {withRouter} from 'react-router-dom';
 
-
+@withRouter
 @inject('appStore')
 @observer
 export default class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isHome: (this.props.history.location.pathname === '/')
+    };
+
+    this.renderBackOption = this.renderBackOption.bind(this);
+    this.backClickHandler = this.backClickHandler.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const isHome = (this.props.history.location.pathname === '/');
+    if (prevState.isHome !== isHome) {
+      this.setState({isHome: isHome});
+    }
+  }
+
+  backClickHandler() {
+    this.props.history.replace('/');
+  }
+
+  renderBackOption() {
+    if (this.state.isHome) {
+      return this.props.appStore.title;
+    } else {
+      return <Button onClick={this.backClickHandler} color="secondary">Back to Search</Button>;
+    }
   }
 
   render() {
@@ -25,8 +50,8 @@ export default class Header extends Component {
         <div className={styles.headerContent}>
           <Container fluid>
             <Row>
-              <Col xs="9">
-                {this.props.appStore.title}
+              <Col xs="9" className="align-self-center">
+                {this.renderBackOption()}
               </Col>
               <Col xs="3">
                 <ClockTime/>
